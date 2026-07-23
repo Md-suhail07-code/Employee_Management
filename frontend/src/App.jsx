48da8114a8f8c87cbf38665c963d1f5d9a3c13aa
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-07-23 21:01:28 +0530

    Created frontend with Vite and initialized tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='text-3xl font-bold text-blue-500'>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
